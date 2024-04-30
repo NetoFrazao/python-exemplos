8086a0fd5dea67b72aeb2af3fd70ffb3011b7fc3
NetoFrazao <joaofrazao054@gmail.com>
2024-04-30 15:09:39 -0300

    Create jo.py

---
+++ jo.py
@@ -0,0 +1,14 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("ola mundo")
+ola mundo
+>>> nome="Joao N"
+>>> idade=33
+>>> email="joaofrazao054@gmail.com"
+>>> telefone: 9764329877
+>>> nota=8.99
+>>> aprovado
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    aprovado
+NameError: name 'aprovado' is not defined
