e62ee7b2e9416a0c26a79073dc599844ad0b1d8a
NetoFrazao <joaofrazao054@gmail.com>
2024-04-30 16:02:18 -0300

    input

---
+++ input.py
@@ -0,0 +1,5 @@
+# Entrada de Dados
+aluno = input ("Digite seu nome: ")
+nota1 = input ("Digite a primeira nota: ")
+nota2 = input ("Digite a segunda nota: ")
+nota3 = input ("Digite a terceira nota: ")
