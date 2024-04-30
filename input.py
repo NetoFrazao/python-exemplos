cd30553c031053aa2081c24e58b3b18a2cc87d24
NetoFrazao <joaofrazao054@gmail.com>
2024-04-30 16:30:45 -0300

    criaçao input.py

--- input.py
+++ input.py
@@ -1,5 +1,10 @@
 # Entrada de Dados
 aluno = input ("Digite seu nome: ")
-nota1 = input ("Digite a primeira nota: ")
-nota2 = input ("Digite a segunda nota: ")
-nota3 = input ("Digite a terceira nota: ")
+nota1 = float(input ("Digite a primeira nota: "))
+nota2 = float(input ("Digite a segunda nota: "))
+nota3 = float(input ("Digite a terceira nota: "))
+falta = int(input ("Digite a quantidade de faltas: "))
+media = (nota1+nota2+nota3)/3
+print("aluno: " + aluno)
+print("media: " + str(media))
+print("falta: " + str(falta))
