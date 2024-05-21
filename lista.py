453ec503e06fb3b88950d43d051b079fd0907542
NetoFrazao <joaofrazao054@gmail.com>
2024-05-21 14:51:53 -0300

    Update lista.py

---
+++ lista.py
@@ -0,0 +1,8 @@
+aluno = ["Joao", 38, 5, False]
+print(f"Nome: {aluno[0]}")
+print(f"Faltas: {aluno[1]}")
+print(f"Nota: {aluno[2]}")
+if aluno[3] == True:
+    print(f"Aprovado: Sim")
+else:
+    print(f"Aprovado: Nao")
