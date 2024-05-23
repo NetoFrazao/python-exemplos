5c4d230fe5663e47090a73e52872f7c014fb84ff
NetoFrazao <joaofrazao054@gmail.com>
2024-05-23 10:33:38 -0300

    Update lista_contar_elementos.py

---
+++ lista_contar_elementos.py
@@ -0,0 +1,4 @@
+alunos=["Adriana", "Joao", "Maria", "Pedro", "Ana", "Lucas", "Mariana", "Adriana"]
+
+ocorrencias = alunos.count("Adriana")
+print(f"Quantidade de ocorrencias de Adrianas: {ocorrencias}")
