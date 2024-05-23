e1655c65b9bd32ad7fe3890711eb290447aaece0
NetoFrazao <joaofrazao054@gmail.com>
2024-05-23 09:40:24 -0300

    Create listas_clonar.py

---
+++ listas_clonar.py
@@ -0,0 +1,6 @@
+alunos=["Joao", "Maria", "Felipe", "Ana", "Lucas", "Mariana"]
+copia = list(alunos)
+print(f"Lista original: {alunos}")
+print(f"Lista copia: {copia}")
+copia.remove("Mariana")
+print(f"Lista de alunos: {copia}")
