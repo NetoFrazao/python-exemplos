3a3c560c08c7412b92cf4dd26af9f749bd584010
NetoFrazao <joaofrazao054@gmail.com>
2024-05-23 09:24:04 -0300

    Create lista_remover.py

---
+++ lista_remover.py
@@ -0,0 +1,11 @@
+alunos=["Adriana", "Joao", "Maria", "Pedro", "Ana", "Lucas", "Mariana", "Adriana"]
+print(f"Lista de alunos: {alunos}")
+# Metodo remove()
+alunos.remove("Adriana")
+print(f"Lista de alunos: {alunos}")
+# Metodo pop()
+alunos.pop(2)
+print(f"Lista de alunos: {alunos}")
+# Funcao del
+del alunos[3]
+print(f"Lista de alunos: {alunos}")
